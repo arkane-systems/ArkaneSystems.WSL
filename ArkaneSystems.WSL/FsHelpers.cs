5526e222eec7a8f11c4efff5927dd46ecc3972e9
Alistair Young <avatar@arkane-systems.net>
2021-08-07 12:49:21 -0500

    Added symbolic link helper.

---
+++ ArkaneSystems.WSL/FsHelpers.cs
@@ -0,0 +1,56 @@
+﻿#region header
+
+// ArkaneSystems.WSL - FsHelpers.cs
+// 
+// Created by: Alistair J R Young (avatar) at 2021/08/07 12:08 PM.
+
+#endregion
+
+#region using
+
+using System;
+using System.Text;
+
+using JetBrains.Annotations;
+
+using Tmds.Linux;
+
+#endregion
+
+namespace ArkaneSystems.WindowsSubsystemForLinux
+{
+    /// <summary>
+    ///     Helper functions for filesystem operations.
+    /// </summary>
+    [PublicAPI]
+    public static class FsHelpers
+    {
+        /// <summary>
+        ///     Create a symbolic link.
+        /// </summary>
+        /// <param name="at">The filesystem location of the symbolic link to be created.</param>
+        /// <param name="to">The filesystem location to which the symbolic link is to point. This need not exist.</param>
+        public static void CreateSymbolicLink (string at, string to)
+        {
+            if (string.IsNullOrEmpty (value: at))
+                throw new ArgumentNullException (paramName: nameof (at),
+                                                 message: "Symbolic link location cannot be null or empty.");
+
+            if (string.IsNullOrEmpty (value: to))
+                throw new ArgumentNullException (paramName: nameof (to),
+                                                 message: "Symbolic link destination cannot be null or empty.");
+
+            unsafe
+            {
+                fixed (byte* bAt = Encoding.UTF8.GetBytes (s: at))
+                fixed (byte* bTo = Encoding.UTF8.GetBytes (s: to))
+                {
+                    if (LibC.symlink (target: bTo, linkpath: bAt) == 0) return;
+
+                    // Error handling.
+                    Helpers.ConvertErrnoToException (errno: LibC.errno);
+                }
+            }
+        }
+    }
+}
---
+++ ArkaneSystems.WSL/Helpers.cs
@@ -0,0 +1,57 @@
+﻿#region header
+
+// ArkaneSystems.WSL - Helpers.cs
+// 
+// Created by: Alistair J R Young (avatar) at 2021/08/07 12:44 PM.
+
+#endregion
+
+#region using
+
+using System;
+using System.Text;
+
+using Tmds.Linux;
+
+#endregion
+
+namespace ArkaneSystems.WindowsSubsystemForLinux
+{
+    /// <summary>
+    ///     Internal helper functions for this library.
+    /// </summary>
+    internal static class Helpers
+    {
+        /// <summary>
+        ///     Convert a Linux error code to a .NET exception.
+        /// </summary>
+        /// <param name="errno">The error code received.</param>
+        internal static void ConvertErrnoToException (int errno)
+        {
+            byte[] messageBuf = new byte[1024];
+
+            unsafe
+            {
+                fixed (byte* bMessage = messageBuf)
+                {
+                    if (LibC.strerror_r (errnum: errno, buf: bMessage, buflen: 1024) != 0)
+                    {
+                        var oops =
+                            new InvalidOperationException (message: $"Unable to translate error message. Sorry. errno={errno}");
+
+                        oops.Data[key: "errno"] = errno;
+
+                        throw oops;
+                    }
+                }
+            }
+
+            string message = Encoding.UTF8.GetString (bytes: messageBuf);
+
+            var ex = new InvalidOperationException (message: message);
+            ex.Data[key: "errno"] = errno;
+
+            throw ex;
+        }
+    }
+}
